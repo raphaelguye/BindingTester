c2c5cbdb3b64cd10310ac755a98ed67f6e64f2ae
Raphael Guye <raphael.guye@gmail.com>
2021-10-18 09:14:26 +0200

    Initial Commit

---
+++ BindingTester/BindingTesterApp.swift
@@ -0,0 +1,15 @@
+//
+// Created by Raphael Guye on 18.10.21
+// https://github.com/raphaelguye
+// 
+
+import SwiftUI
+
+@main
+struct BindingTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
