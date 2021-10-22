eb10fff21f10314978d3c61a7600694b3443bb6f
Raphael Guye <raphael.guye@gmail.com>
2021-10-22 08:04:42 +0200

    Fix unit tests. ref: https://forums.swift.org/t/unit-test-binding-properties/52932/5

--- BindingTesterTests/SheetViewModelTest.swift
+++ BindingTesterTests/SheetViewModelTest.swift
@@ -3,33 +3,45 @@ import SwiftUI
 
 class SheetViewModelTest: XCTestCase {
 
-  func testBindingWithConstant() {
+  func testWithBindingSetter() {
 
-    let viewModel = SheetViewModel(isPresented: .constant(true))
+    var isPresentedBindingValue = true
+    let isPresentedBinding = Binding(get: { isPresentedBindingValue }, set: { isPresentedBindingValue = $0 })
+    let viewModel = SheetViewModel(isPresented: isPresentedBinding)
 
-    XCTAssertTrue(viewModel.isPresented)
+    XCTAssertTrue(isPresentedBindingValue)
     viewModel.onClose()
-    XCTAssertFalse(viewModel.isPresented)
+    XCTAssertFalse(isPresentedBindingValue)
   }
 
-  func testBindingWithState() {
-
-    @State var isSheetDisplayed: Bool = true
-    let viewModel = SheetViewModel(isPresented: $isSheetDisplayed)
-
-    XCTAssertTrue(viewModel.isPresented)
-    viewModel.onClose()
-    XCTAssertFalse(viewModel.isPresented)
-  }
-
-  func testBindingWithBindingObject() {
-
-    let isSheetDisplayed: Binding<Bool> = .constant(true)
-    let viewModel = SheetViewModel(isPresented: isSheetDisplayed)
-
-    XCTAssertTrue(viewModel.isPresented)
-    viewModel.onClose()
-    XCTAssertFalse(viewModel.isPresented)
-  }
+  // Not Working use cases:
+//  func testBindingWithConstant() {
+//
+//    let viewModel = SheetViewModel(isPresented: .constant(true))
+//
+//    XCTAssertTrue(viewModel.isPresented)
+//    viewModel.onClose()
+//    XCTAssertFalse(viewModel.isPresented)
+//  }
+//
+//  func testBindingWithState() {
+//
+//    @State var isSheetDisplayed: Bool = true
+//    let viewModel = SheetViewModel(isPresented: $isSheetDisplayed)
+//
+//    XCTAssertTrue(viewModel.isPresented)
+//    viewModel.onClose()
+//    XCTAssertFalse(viewModel.isPresented)
+//  }
+//
+//  func testBindingWithBindingObject() {
+//
+//    let isSheetDisplayed: Binding<Bool> = .constant(true)
+//    let viewModel = SheetViewModel(isPresented: isSheetDisplayed)
+//
+//    XCTAssertTrue(viewModel.isPresented)
+//    viewModel.onClose()
+//    XCTAssertFalse(viewModel.isPresented)
+//  }
 
 }
